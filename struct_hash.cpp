706595a77b736e8e785cb9cfaddf840f6b72c01f
Mai Quốc Bảo <104915677+baoq06@users.noreply.github.com>
2024-10-20 07:53:34 +0700

    Update struct_hash.cpp

--- struct_hash.cpp
+++ struct_hash.cpp
@@ -1,22 +1,32 @@
-struct Hash
-{
-    int n;
-    vector<ll> h; 
-    Hash(int _n)
-    {
-        h = vector<ll>(_n + 1, 0);
-        n = _n;
+const int N = 1e6;
+const int BASE = 311;
+const int MOD = 1e9 + 7;
+string s, t;
+
+struct hash_string {
+    vector<int> hash, power;
+    int n, BASE, MOD;
+    string s;
+    hash_string(string _s = "", int _BASE = 0, int _MOD = 0) {
+        s =  ' ' + _s;
+        n = s.size() - 1;
+        BASE = _BASE;
+        MOD = _MOD;
+        hash.assign(n + 1, 0);  
+        power.assign(N + 1, 0);
     }
-    
-    void buildHash(string s, int size)
-    {
-        h[0] = 0;
-        for(int i = 1; i <= size; i++) 
-            h[i] = (h[i - 1] + 1LL * s[i - 1] * POW[i]) % MOD;
+
+    void build() {
+        power[0] = 1;
+        for(int i = 1; i <= N; i++)
+            power[i] = (1LL * power[i - 1] * BASE) % MOD;
+
+        hash[0] = 0;
+        for(int i = 1; i <= n; i++)
+            hash[i] = (hash[i - 1] + 1LL * power[i] * s[i]) % MOD;  
     }
-    
-    ll getHash(int i, int j)
-    {
-        return 1LL * (h[j] - h[i - 1] + MOD) * POW[n - j] % MOD;
+
+    int get_hash(int l, int r) {
+        return 1LL * (hash[r] - hash[l - 1] +  MOD) * power[N - r] % MOD;
     }
 };
