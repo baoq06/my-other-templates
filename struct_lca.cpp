55ffad5c72ca61f81eef1aa0616eff0faf163547
Mai Quốc Bảo <104915677+baoq06@users.noreply.github.com>
2024-10-20 07:09:49 +0700

    Update struct_lca.cpp

--- struct_lca.cpp
+++ struct_lca.cpp
@@ -1,50 +1,48 @@
-struct LCA
-{
-    vector<vector<int>> parent;
-    vector<int> depth;
+struct lowest_common_ancestor {
     int n;
+    vector<int> dep;
+    vector<vector<int>> par;
 
-    LCA(const int& _n = 0)
-    {
+    lowest_common_ancestor(int _n = 0) {
         n = _n;
-        parent.assign(n + 1, vector<int>(LOG + 1, 0));
-        depth.assign(n + 1, 0);
-        depth[0] = -1;
+        dep.assign(n + 1, 1); dep[0] = 0;
+        par.assign(n + 1, vector<int>(LOG + 1, 0));
     }
 
-    void assignDepth(int u)
-    {
-        for(auto& v : adj[u])
-        {
-            if(v == parent[u][0]) continue;
-            parent[v][0] = u;
-            depth[v] = depth[u] + 1;
-            assignDepth(v);
-        }
+    void assign_depth(int cur) {
+        for(auto& next : adj[cur]) 
+            if(next != par[cur][0]) {
+                par[next][0] = cur;
+                dep[next] = dep[cur] + 1;
+                assign_depth(next);
+            }
     }
 
-    void binLifting()
-    {
+    void binary_lifting() {
         for(int j = 1; j <= LOG; j++)
             for(int i = 1; i <= n; i++)
-                parent[i][j] = parent[parent[i][j - 1]][j - 1];
+                par[i][j] = par[par[i][j - 1]][j - 1];
     }
 
-    void process() {assignDepth(1); binLifting();}
-    int query(int u, int v)
-    {
-        if(depth[u] < depth[v]) return query(v, u);
+    void build() {
+        assign_depth(1);
+        binary_lifting();
+    }
+
+    int get_lca(int x, int y) {
+        if(dep[x] < dep[y]) return get_lca(y, x);
         for(int i = LOG; i >= 0; i--)
-            if(depth[parent[u][i]] >= depth[v]) u = parent[u][i];
-        
-        if(u == v) return u;
+            if(dep[par[x][i]] >= dep[y])
+                x = par[x][i];
+
+        if(x == y)  return x;
 
         for(int i = LOG; i >= 0; i--)
-            if(parent[u][i] != parent[v][i])
-            {
-                u = parent[u][i];
-                v = parent[v][i];
+            if(par[x][i] != par[y][i]) {
+                x = par[x][i];
+                y = par[y][i];
             }
-        return parent[u][0];
+
+        return par[x][0];
     }
 };
